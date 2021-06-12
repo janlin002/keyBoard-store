9498d8b02a4eae86dc69318fe6a7197dd710aecf
janlin <123davidbill@gmail.com>
2021-06-12 15:26:51 +0800

    hashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter, Switch, Route } from 'react-router-dom';
 
 // component
 import Product from './Component/Product Info/Product';
@@ -20,6 +20,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <HashRouter>
         <Router>
           <Navbars />
           <Carousel />
@@ -45,6 +46,7 @@ class App extends React.Component {
           </Switch>
           <Footer />
         </Router>
+        </HashRouter>
       </div>
     );
   }
